621962b4e5ee179a3838bd1f0c4b3199da3aeb79
MrCristianrl <41384256+MrCristianrl@users.noreply.github.com>
2018-07-18 17:54:16 +0200

    Create wordguessed.py

---
+++ wordguessed.py
@@ -0,0 +1,20 @@
+First, implement the function isWordGuessed that takes in two parameters - a string, secretWord, and a list of letters, lettersGuessed. This function returns a boolean - True if secretWord has been guessed (ie, all the letters of secretWord are in lettersGuessed) and False otherwise.
+
+
+
+def isWordGuessed(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: boolean, True if all the letters of secretWord are in lettersGuessed;
+      False otherwise
+    '''
+    # FILL IN YOUR CODE HERE...
+    count = 0
+    for c in secretWord:
+        if c in lettersGuessed:
+            count += 1
+    if count == len(secretWord):
+        return True
+    else:
+        return False
